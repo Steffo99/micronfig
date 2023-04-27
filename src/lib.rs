28a4371f1d9bcf61b07452546c1ca426b643cb75
Stefano Pigozzi <me@steffo.eu>
2023-04-27 18:16:49 +0000

    Create the library

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::env;
+use std::ffi::OsString;
+use std::io::Read;
+use std::path::PathBuf;
+use std::fs::File;
+
+/// An error that occurred while getting a configuration value.
+pub enum GetFileError<TargetType> where TargetType: TryFrom<String> {
+    CannotReadEnvVar(std::env::VarError),
+    CannotOpenFile(std::io::Error),
+    CannotReadFile(std::io::Error),
+    CannotConvertValue(<TargetType as TryFrom<String>>::Error),
 }
+
+/// The result of an attempt at getting a configuration value.
+pub type GetFileResult<TargetType> = Result<TargetType, GetFileError<TargetType>>;
+
+/// Get a configuration value from a file specified in the given environment variable.
+/// 
+///
+/// # Parameters
+///
+/// - `name`: the name of the environment variable containing the location of the file containing the configuration value.
+/// - `TargetType`: the struct the contents of the file are to be converted into using [`TryFrom`] [`String`].
+/// 
+pub fn get_file<TargetType>(name: &str) -> GetFileResult<TargetType> where TargetType: TryFrom<String> {
+    let path = env::var(name).map_err(GetFileError::CannotReadEnvVar)?;
+    let path = OsString::from(path);
+    let path = PathBuf::from(path);
+
+    let mut file = File::open(path).map_err(GetFileError::CannotOpenFile)?;
+    
+    let mut data = String::new();
+    file.read_to_string(&mut data).map_err(GetFileError::CannotReadFile)?;
+
+    let value = TargetType::try_from(data).map_err(GetFileError::CannotConvertValue)?;
+
+    Ok(value)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +45,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        todo!()
     }
 }
